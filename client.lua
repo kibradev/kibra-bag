5b90cfb732201861d815dd67494e374ab66cb102
kibra <62299912+kibradev@users.noreply.github.com>
2021-07-16 15:20:04 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -9,8 +9,8 @@ end)
 
 local can = math.random(1,6000) 
 
-RegisterNetEvent("K8.CNT.USE")
-AddEventHandler("K8.CNT.USE", function(id)
+RegisterNetEvent("KIBRA.CNT.USE")
+AddEventHandler("KIBRA.CNT.USE", function(id)
   TriggerServerEvent("inventory:server:OpenInventory", "stash", "Canta_"..id, {
     maxweight = 15,
     slots = 15,
