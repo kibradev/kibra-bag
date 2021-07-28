31c1724590146d7b1a4403898529e30d85ee5a15
Jagen0 <79335555+Jagen0@users.noreply.github.com>
2021-07-29 00:07:34 +0300

    Code Clear

--- client.lua
+++ client.lua
@@ -1,21 +1,8 @@
-ESX = nil
-
-Citizen.CreateThread(function()
-  while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-    Citizen.Wait(31)
-  end
-end)
-
-local can = math.random(1,6000) 
-
 RegisterNetEvent("KIBRA.CNT.USE")
 AddEventHandler("KIBRA.CNT.USE", function(id)
-  TriggerServerEvent("inventory:server:OpenInventory", "stash", "Canta_"..id, {
-    maxweight = 15,
-    slots = 15,
-})
-TriggerEvent("inventory:client:SetCurrentStash", "Canta_"..id)
+  TriggerServerEvent("inventory:server:OpenInventory", "stash", "Canta_" .. id, {
+        maxweight = 15,
+        slots = 15,
+  })
+  TriggerEvent("inventory:client:SetCurrentStash", "Canta_" .. id)
 end)
-
-
