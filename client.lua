1d5e70b5c69865335d5e5460579ce37cea605a1b
kibra <62299912+kibradev@users.noreply.github.com>
2021-12-25 12:25:49 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,1 @@
-RegisterNetEvent('kibra-canta:client:UseBag', function(cantaid)
-    TriggerServerEvent("inventory:server:OpenInventory", "stash", "Bag_"..tonumber(cantaid))
-    TriggerEvent("inventory:client:SetCurrentStash", "Bag_"..tonumber(cantaid))
-end)
+RegisterNetEvent('kibra-canta:client:UseBag', function(cantaid) TriggerServerEvent("inventory:server:OpenInventory", "stash", "Bag_"..tonumber(cantaid)) TriggerEvent("inventory:client:SetCurrentStash", "Bag_"..tonumber(cantaid) end)
