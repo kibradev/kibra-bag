94b325ace1280377e0f89f619136eb10de0df6ce
kibra <62299912+kibradev@users.noreply.github.com>
2021-12-25 10:56:31 +0300

    Fixed

--- client.lua
+++ client.lua
@@ -1,8 +1,4 @@
-RegisterNetEvent("KIBRA.CNT.USE")
-AddEventHandler("KIBRA.CNT.USE", function(id)
-  TriggerServerEvent("inventory:server:OpenInventory", "stash", "Canta_" .. id, {
-        maxweight = 15,
-        slots = 15,
-  })
-  TriggerEvent("inventory:client:SetCurrentStash", "Canta_" .. id)
+RegisterNetEvent('kibra-canta:client:UseBag', function(cantaid)
+    TriggerServerEvent("inventory:server:OpenInventory", "stash", "Bag_"..tonumber(cantaid))
+    TriggerEvent("inventory:client:SetCurrentStash", "Bag_"..tonumber(cantaid))
 end)
