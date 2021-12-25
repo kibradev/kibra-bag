3b992d69ae78c4c554991d86d505f65e7433abd8
kibra <62299912+kibradev@users.noreply.github.com>
2021-12-25 12:26:24 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,12 +1,6 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
+ESX = nil TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 ESX.RegisterUsableItem('cantax', function(source,item) end)
-
 RegisterCommand('givebag', function(source, args)
-    local Player = ESX.GetPlayerFromId(source)
-    local info = {
-        cantaid = math.random(1111111111,9999999999)
-    }
-    Player.addInventoryItem('cantax', 1, nil, info)
+    local info = {cantaid = math.random(1111111111,9999999999)}
+    ESX.GetPlayerFromId(source).addInventoryItem('cantax', 1, nil, info)
 end)
